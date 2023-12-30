836565fcaffc01d8dd492857070f39beffbc573d
umigitumi <145943683+umigitumi@users.noreply.github.com>
2023-12-30 18:18:08 +0900

    コメントに関する修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -47,16 +47,17 @@ class PrototypesController < ApplicationController
     redirect_to root_path
   end
 
-  def move_to_index
-    unless user_signed_in?
-      redirect_to action: :index
-    end
-  end
-
 
   private
 
   def prototype_params
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
   end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
+  end
+
 end
